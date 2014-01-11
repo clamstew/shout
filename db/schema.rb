04d21453a1d54814178e48ecb5c6185ff7d56713
Gilbert <gilbertbgarza@gmail.com>
2014-01-11 15:17:38 -0600

    Move from sqlite3 to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140111202215) do
+ActiveRecord::Schema.define(version: 20140111205814) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "links", force: true do |t|
     t.string   "url"
